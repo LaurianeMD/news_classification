e4e7fd239f20bdce037e94a8919113f751e1a4ff
Lauriane <dmbagdjelauriane@gmail.com>
2024-07-10 15:13:48 +0000

    add main.py and utils.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# main.py
+
+import gradio as gr
+from utils import load_model, predict_category
+from news_dataset import NewsDataset  # Importez NewsDataset depuis news_dataset.py
+
+def launch_app():
+    dataset = NewsDataset(csv_file="./inshort_news_data.csv", max_length=100)
+    num_classes = len(dataset.labels_dict)
+    model_path = './models/my_custom_bert.pth'  # Chemin vers le modèle entraîné
+    model = load_model(model_path, num_classes)  # Charger le modèle entraîné avec le bon nombre de classes
+
+    labels_dict = dataset.labels_dict
+
+    def predict_function(headline, article):
+        return predict_category(headline, article, model, labels_dict)
+
+    iface = gr.Interface(
+        fn=predict_function,
+        inputs=["text", "text"],
+        outputs="text",
+        title="News Category Classification",
+        description="Enter a headline and an article to classify its category."
+    )
+
+    iface.launch(share=True)
+
+if __name__ == "__main__":
+    launch_app()
---
+++ utils.py
@@ -0,0 +1,35 @@
+import torch
+from transformers import AutoTokenizer
+import torch.nn as nn
+from bert_classification import CustomBert  # Importer le modèle depuis le fichier bert_classification.py
+
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+
+def load_model(model_path, num_classes):
+    model = CustomBert(n_classes=num_classes)  # Adapter ici le nombre de classes
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    return model
+
+def predict_category(headline, article, model, labels_dict, max_length=100):
+    text = headline + " " + article
+    inputs = tokenizer(
+        text,
+        padding="max_length",
+        max_length=max_length,
+        truncation=True,
+        return_tensors="pt"
+    )
+
+    with torch.no_grad():
+        output = model(input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"])
+        probabilities = nn.Softmax(dim=1)(output)
+        _, pred = torch.max(probabilities, dim=1)
+        score = probabilities[0][pred].item()
+
+        inv_labels_dict = {v: k for k, v in labels_dict.items()}
+        category = inv_labels_dict[pred.item()]
+
+    score = round(score*100, 2) 
+
+    return category, score
